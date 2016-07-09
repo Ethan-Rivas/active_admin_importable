68e25d8e99e20863d42189284ec4bc35d8043221
Ethan-Rivas <ethan-rivas@hotmail.com>
2016-07-08 19:23:16 -0500

    change to spanish

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -2,7 +2,7 @@ module ActiveAdminImportable
   module DSL
     def active_admin_importable(&block)
       action_item :only => :index do
-        link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'upload_csv'
+        link_to "Importar", :action => 'upload_csv'
       end
 
       collection_action :upload_csv do
@@ -11,7 +11,7 @@ module ActiveAdminImportable
 
       collection_action :import_csv, :method => :post do
         CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], &block)
-        redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
+        redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} importados!"
       end
     end
   end
